0fd3e291fb319a38adf4a3fef530ac1531351353
Paul JULLIEN <paul_jullien@outlook.fr>
2022-10-23 18:11:58 +0200

    separate debug function from the rest of the app code

---
+++ srcs/debug.py
@@ -0,0 +1,11 @@
+def debug_environ(environ, start_response):
+    response_body = [f"{key}:{value}" for key, value in environ.items()]
+    response_body = "\n".join(response_body)
+    status = '200 OK'
+    response_headers = [
+        ('Content-Type', 'text/plain'),
+        ('Content-Length', str(len(response_body)))
+    ]
+
+    start_response(status, response_headers)
+    return [response_body.encode()]
