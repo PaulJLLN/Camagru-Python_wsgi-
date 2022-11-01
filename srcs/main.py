110d3b769edceaca61231503708d2923a7b5d042
Paul JULLIEN <paul_jullien@outlook.fr>
2022-11-01 16:24:21 +0100

    Adding a proper way to redirect to notfound route.

--- srcs/main.py
+++ srcs/main.py
@@ -1,6 +1,8 @@
 from wsgiref.simple_server import make_server
 
-from views.View import NotFoundView, SignInView, SignUpView
+from views.signin import SignInView
+from views.signup import SignUpView
+from views.notfound import NotFoundView
 
 PORT = 8051
 HOST = "0.0.0.0"
@@ -8,18 +10,24 @@ HOST = "0.0.0.0"
 ROUTES = {
     "/": SignInView,
     "/signup": SignUpView,
-    "404": NotFoundView,
+    "/notfound": NotFoundView,
 }
 
 def application(environ, start_response):
+    print(f"PATH_INFO : {environ['PATH_INFO']}")
     for path in ROUTES:
+        print(path)
         if (environ["PATH_INFO"] == path):
-            instance = ROUTES[path](environ, start_response)
+            if (path != "/notfound"):
+                instance = ROUTES[path](environ, start_response)
+            else:
+                instance = ROUTES[path](start_response)
             if (environ["REQUEST_METHOD"] == "GET"):
                 return instance.do_GET()
             elif (environ["REQUEST_METHOD"] == "POST"):
                 return instance.do_POST()
-    return ROUTES['404'](start_response).do_GET()
+    print("Ca part sur du 404")
+    return ROUTES['/notfound'](start_response).redirect_to_not_found()
     
 
 if __name__ == "__main__":
--- srcs/views/notfound.py
+++ srcs/views/notfound.py
@@ -8,9 +8,22 @@ class NotFoundView:
             response_body = index.read()
         response_headers = [
             ('Content-Type', 'text/html'),
-            ('Content-Length', str(len(response_body)))
+            ('Content-Length', str(len(response_body))),
         ]
 
+        self._start_fn(status, response_headers)
+        return [response_body.encode()]
+
+    def redirect_to_not_found(self):
+        print("redirect to not found")
+        status = "302 See Other"
+        with open("templates/notFound.html", "r") as index:
+            response_body = index.read()
+        response_headers = [
+            ('Content-Type', 'text/html'),
+            ('Content-Length', str(len(response_body))),
+            ('Location', "http://localhost:8051/notfound")
+        ]
         self._start_fn(status, response_headers)
         return [response_body.encode()]
     
